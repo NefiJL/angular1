da78f8b4e1664b7fcfadef8ec564b25091675ded
NefiJL <116108711+NefiJL@users.noreply.github.com>
2023-03-07 15:06:15 -0300

    Aula1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeucomponenteComponent } from './novo/component/meucomponente/meucomponente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeucomponenteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/novo/component/meucomponente/meucomponente.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meucomponente',
+  templateUrl: './meucomponente.component.html',
+  styleUrls: ['./meucomponente.component.scss']
+})
+export class MeucomponenteComponent implements OnInit {
+  //ATRIBUTOS
+
+  private nome: string;
+  anoNascimento: number; //public e padrão
+
+  //CONSTRUCTOR
+  constructor() {
+    this.nome = "Fulano da Silva"
+    this.anoNascimento = 2005
+  }
+
+  //COMPORTAMENTO AÇÕES
+    idade (): number{
+      return 2023- this.anoNascimento;
+    }
+
+    ngOnInit(): void{
+      console.log("Nome:" + this.nome);
+      console.log("Idade:" + this.idade());
+    }
+  }
+
+
